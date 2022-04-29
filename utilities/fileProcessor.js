6b779df30b64b1987e6434a8e9f683bd3d328937
sorted-hd <hdubey71@gmail.com>
2022-04-29 21:48:57 +0530

    Refactored v1

--- utilities/fileProcessor.js
+++ utilities/fileProcessor.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 
 const fileProcessor = (filePath) => {
-  if (!fs.existsSync(filePath)) {
-    console.log(
-      'No input file found at given path. Please provide a correct file path'
-    );
-    return;
-  }
+  // if (!fs.existsSync(filePath)) {
+  //   console.log(
+  //     'No input file found at given path. Please provide a correct file path'
+  //   );
+  //   return;
+  // }
   let inputData = fs.readFileSync(filePath, { flag: 'r' });
   inputData = inputData.toString().trim().split('\n');
   return inputData;
